c36ad0c986305fcf37b84e0ea4f52a254f6da773
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-10 17:07:07 +0330

    Write getter setter Constructor

--- src/main/java/employeeadress/EmployessAddress.java
+++ src/main/java/employeeadress/EmployessAddress.java
@@ -1,4 +1,47 @@
 package employeeadress;
 
 public class EmployessAddress {
+    private Integer addressId;
+    private String city;
+    private String street;
+    private int postCode;
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(int postCode) {
+        this.postCode = postCode;
+    }
+
+    public EmployessAddress(Integer addressId, String city, String street, int postCode) {
+        this.addressId = addressId;
+        this.city = city;
+        this.street = street;
+        this.postCode = postCode;
+    }
 }
