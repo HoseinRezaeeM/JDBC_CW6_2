7fc10b38b34ca5fdcd59042875596b34d274e4f1
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-11 13:43:34 +0330

    Write Connection

--- src/main/java/jdbcConnetion/JdbcConnection.java
+++ src/main/java/jdbcConnetion/JdbcConnection.java
@@ -1,4 +1,19 @@
 package jdbcConnetion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+
+
+    Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+            "postgres", "173725Hrm");
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public JdbcConnection() throws SQLException {
+    }
 }
