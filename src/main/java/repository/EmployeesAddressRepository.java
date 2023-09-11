8cd3e545660bfc0ad090d013491b4406c573c338
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-11 13:44:17 +0330

    Write method update and delete

--- src/main/java/repository/EmployeesAddressRepository.java
+++ src/main/java/repository/EmployeesAddressRepository.java
@@ -1,4 +1,55 @@
 package repository;
 
+
+import employeeadress.EmployessAddress;
+import employees.Employees;
+import jdbcConnetion.JdbcConnection;
+
+import java.sql.*;
+
 public class EmployeesAddressRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public EmployeesAddressRepository() throws SQLException {
+    }
+
+    public void createTable() throws SQLException {
+        String sql = "CREATE TABLE employeesaddress (" +
+                "addressId serial references employee(addressid)," +
+                "city varchar(50)," +
+                "street varchar(50)," +
+                "postalCode int)";
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sql);
+
+    }
+
+    public void select(int employeeNumber) throws SQLException {
+        String sql = "SELECT employeesaddress.city ,employeesaddress.street FROM " +
+                "employeesaddress inner join employee e on e.addressid = employeesaddress.addressid where employessnumber= ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, employeeNumber);
+        ResultSet result = preparedStatement.executeQuery();
+        if (result.next()) {
+            String city = result.getString("city");
+            String street = result.getString("street");
+            System.out.println("Address: " + city + ", " + street);
+        }
+    }
+
+        public int Delete(int addressid)throws SQLException {
+            String sqll = "DELETE FROM employeesaddress WHERE addressid=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqll);
+            preparedStatement.setInt(1, addressid);
+            int result = preparedStatement.executeUpdate();
+            return result;
+        }
+
+
+
+
+
 }
+
